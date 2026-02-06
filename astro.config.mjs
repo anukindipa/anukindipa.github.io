df900982a7a5265b6691d279289aae8555b86b3d
Anuk Indipa <61876990+anukindipa@users.noreply.github.com>
2026-02-06 16:32:36 +1100

    Add sitemap.xml for google console

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import remarkCallout from "@r4ai/remark-callout";
@@ -8,10 +10,11 @@ import remarkCallout from "@r4ai/remark-callout";
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    remarkPlugins: [remarkCallout, remarkMath, ],
+    remarkPlugins: [remarkCallout, remarkMath,],
     rehypePlugins: [rehypeKatex,],
     // kanagawa-dragon, catppuccin-mocha, github-dark-default looks nice too
     shikiConfig: { theme: 'catppuccin-mocha' },
   },
   site: 'https://anukindipa.github.io',
+  integrations: [sitemap()],
 });
